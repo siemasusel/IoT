f8c6c251eaa54df586eea6f3920f1a763075a2d5
root <root@localhost.localdomain>
2020-11-11 14:55:13 -0500

    Add example handlers

--- app/entry.go
+++ app/entry.go
@@ -2,14 +2,12 @@ package main
 
 // ssh -Llocalhost:8086:10.72.1.106:8086 jtomasik@pluton.kt.agh.edu.pl
 import (
-	"fmt"
-
 	"github.com/siemasusel/IoT/collector"
-	"github.com/siemasusel/IoT/reciever"
+	"github.com/siemasusel/IoT/collector/handlers"
 )
 
 func main() {
-	reciever.Run()
-	collector.Run()
-	fmt.Println("Hello, world.")
+	app := collector.MakeCollector("localhost:8086", map[string]string{"device_id": "999"}, "60s")
+	app.AddMetric(handlers.MakeTempertatureMetric())
+	app.RunLoop()
 }
