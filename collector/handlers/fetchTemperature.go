9fa53c98fa8cf365b821109f2aedafb55706d892
siemasusel <jantomx@gmail.com>
2020-11-12 11:43:43 +0100

    Update fetchTemperature.go

--- collector/handlers/fetchTemperature.go
+++ collector/handlers/fetchTemperature.go
@@ -9,5 +9,5 @@ func FetchTemperature() (interface{}, error) {
 }
 
 func MakeTempertatureMetric() collector.Metric {
-	return collector.MakeMetric("temperature", FetchTemperature, map[string]string{"unit": "cellsius"})
+	return collector.MakeMetric("temperature", FetchTemperature, map[string]string{"unit": "celsius"})
 }
