2bba0d07e50913f4b4a03302e76aa83131898562
kotmich <kotmich97@gmail.com>
2020-11-25 14:28:17 +0100

    Receiver function initial implementation fix

--- reciever/main.go
+++ reciever/main.go
@@ -1,7 +1,26 @@
 package reciever
 
-import "fmt"
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+)
 
 func Run() {
 	fmt.Println("Hello, world from reciever.")
+}
+
+func show(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "GET" {
+		t, _ := template.ParseFiles("show.gtpl")
+		t.Execute(w, nil)
+	} else {
+		r.ParseForm()
+		fmt.Println(r.Form["task"])
+	}
+}
+
+func main() {
+	http.HandleFunc("/show", show)
+	http.ListenAndServe(":9000", nil)
 }
