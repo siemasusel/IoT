ee9ccee2ad169262423acb5239f24ee5accff63d
Jan <jantomx@gmail.com>
2021-01-12 12:59:05 -0500

    add install script

--- app/bin/install.sh
+++ app/bin/install.sh
@@ -4,6 +4,8 @@ wget -O /usr/iot/entry_collector https://github.com/siemasusel/IoT/raw/master/ap
 wget -O /usr/iot/entry_receiver https://github.com/siemasusel/IoT/raw/master/app/bin/entry_receiver
 wget -O /etc/systemd/system/iot_collector.service https://raw.githubusercontent.com/siemasusel/IoT/master/systemd/iot_collector.service
 wget -O /etc/systemd/system/iot_receiver.service https://raw.githubusercontent.com/siemasusel/IoT/master/systemd/iot_receiver.service
+chmod 755 /usr/iot/entry_collector
+chmod 755 /usr/iot//usr/iot/entry_receiver
 systemctl start iot_collector
 systemctl enable iot_collector
 systemctl start iot_receiver
