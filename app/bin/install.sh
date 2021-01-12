4941d196a5c10481629fbf570e1eeb3f0470d28f
siemasusel <jantomx@gmail.com>
2021-01-12 19:43:27 +0100

    Update install.sh

--- app/bin/install.sh
+++ app/bin/install.sh
@@ -5,7 +5,7 @@ wget -O /usr/iot/entry_receiver https://github.com/siemasusel/IoT/raw/master/app
 wget -O /etc/systemd/system/iot_collector.service https://raw.githubusercontent.com/siemasusel/IoT/master/systemd/iot_collector.service
 wget -O /etc/systemd/system/iot_receiver.service https://raw.githubusercontent.com/siemasusel/IoT/master/systemd/iot_receiver.service
 chmod 755 /usr/iot/entry_collector
-chmod 755 /usr/iot/usr/iot/entry_receiver
+chmod 755 /usr/iot/entry_receiver
 systemctl daemon-reload
 systemctl start iot_collector
 systemctl enable iot_collector
