dd717cfd7cc93cfc117c17fd9e55200ea7a0badf
Jan <jantomx@gmail.com>
2021-01-12 14:26:44 -0500

    fix bugs 3

--- receiver/main.go
+++ receiver/main.go
@@ -37,13 +37,13 @@ func MakeReceiver(ip string, port int) Receiver {
 
 func TemperatureHandler(w http.ResponseWriter, r *http.Request) {
 	log.Info("Endpoint Hit: TemperatureHandler")
-
+	log.Info("DEBUG: TemperatureHandler")
 	switch r.Method {
 	case "GET":
 		log.Info("GET current Temperature")
 	case "POST":
 		var temperature TempValue
-
+		log.Info("POST: TemperatureHandler")
 		// Try to decode the request body into the struct. If there is an error,
 		// respond to the client with the error message and a 400 status code.
 		err := json.NewDecoder(r.Body).Decode(&temperature)
