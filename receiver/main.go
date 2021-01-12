f2356dc0d83089d315b60f4c29cfb0638ecfffeb
Jan <jantomx@gmail.com>
2021-01-12 14:02:15 -0500

    merge 2

--- receiver/main.go
+++ receiver/main.go
@@ -46,6 +46,7 @@ func TemperatureHandler(w http.ResponseWriter, r *http.Request) {
 
 		// Try to decode the request body into the struct. If there is an error,
 		// respond to the client with the error message and a 400 status code.
+		log.Info(r.Body)
 		err := json.NewDecoder(r.Body).Decode(&temperature)
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusBadRequest)
@@ -104,6 +105,7 @@ func HumidityHandler(w http.ResponseWriter, r *http.Request) {
 	case "POST":
 		var humidity HumidityValue
 
+		log.Info(r.Body)
 		// Try to decode the request body into the struct. If there is an error,
 		// respond to the client with the error message and a 400 status code.
 		err := json.NewDecoder(r.Body).Decode(&humidity)
@@ -149,6 +151,7 @@ func FoodHandler(w http.ResponseWriter, r *http.Request) {
 
 		// Try to decode the request body into the struct. If there is an error,
 		// respond to the client with the error message and a 400 status code.
+		log.Info(r.Body)
 		err := json.NewDecoder(r.Body).Decode(&feed)
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusBadRequest)
